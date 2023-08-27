aa9339b85723da5a26fad014e4bee9ffc7f5ebcb
Colin Beltz <colinbeltz@gmail.com>
2023-08-27 10:41:31 -0500

    WordCounter initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <section>
+     <h1>Word Counter</h1>
+     <h2>Free online character and word count tool</h2>
+      <form action="">
+        <input type="textarea" />
+        <section display="flex"><button class="">Clear</button></section>
+        <section display="flex-between">
+          <button class="">Character:0</button>
+          <button class="">Word:0</button>
+        </section>
+        </form>
+     </section>
+
     </div>
   );
 }
